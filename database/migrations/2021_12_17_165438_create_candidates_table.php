4c3d7e8bdeebf762e083c8b43b9e3075c13be478
dikewarda <dikeayuw@gmail.com>
2023-06-07 22:05:30 +0700

    fixx

---
+++ database/migrations/2021_12_17_165438_create_candidates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_ketua');
+            $table->string('nama_wakil');
+            $table->text('visi');
+            $table->text('misi');
+            $table->string('foto_paslon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
